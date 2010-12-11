b5c95980afbde57d1b7ca5f7cea53964bc87c7ed
Chris Boertien <chris.boertien@gmail.com>
2010-12-10 23:25:22 -0600

    Initial commit
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'spree'
+
+group :development, :test do
+  gem 'compass'
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'spork', '0.9.0.rc2'
+  gem 'rcov'
+  gem 'annotate-models'
+end
+
+group :noload do
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuEraFitness::Application.config.secret_token = '030db4a72d0a596eb53c958aa5dbf0fa7b392a1771bac1c5f6360e39fb9fe8e8aa20d275043098861e14a5e13d68469c0929e5bfb8e38fa21121ebe7e70a8d20'
