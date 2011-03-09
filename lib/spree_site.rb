74f4ccdfa8130f15f508ab457a7b4ff5b7b0ee8f
Chris Boertien <chris.boertien@gmail.com>
2011-03-09 15:28:14 +0000

    setup the engine to load the decorators and calculators, as well as a fix for the double rake bug
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -1,8 +1,22 @@
+require 'site_hooks'
+
 module SpreeSite
   class Engine < Rails::Engine
     def self.activate
       # Add your custom site logic here
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator.rb")) do |f|
+        Rails.configuration.cache_classes ? require(f) : load(f)
+      end
+
+      [
+        Calculator::Ups::Standard,
+        Calculator::Freight
+      ].each(&:register)
+
     end
+
+    def load_tasks; end
+
     config.to_prepare &method(:activate).to_proc
   end
 end
