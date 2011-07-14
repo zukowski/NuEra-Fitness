401a7295fa707c5a3cde460e078ee0d31f07b93f
Chris Boertien <chris.boertien@gmail.com>
2011-07-14 10:24:18 -0700

    Modify production environment to match old environment
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,17 +25,18 @@ NuEraFitness::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :file_store, 'tmp/cache'
+  #config.cache_store = :file_store, 'tmp/cache'
+  config.cache_store = :mem_cache_store, 'localhost'
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = Proc.new { |source|
-    "http://assets-#{rand(2) + 1}.nuerafitness.com"
-  }
+  #config.action_controller.asset_host = Proc.new { |source|
+  #  "http://assets-#{rand(2) + 1}.nuerafitness.com"
+  #}
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
