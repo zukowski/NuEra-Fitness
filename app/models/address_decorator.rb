a2b448a63bbf4ca0f3faca0a162a7b8bbcf1ecd0
Chris Boertien <chris.boertien@gmail.com>
2011-08-26 00:54:56 -0700

    fixup address to better handle state_name cases
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -2,8 +2,11 @@ Address.class_eval do
   before_validation :cleanup_state
 
   def cleanup_state
+    return unless state_name
     state = country.states.where(["name = ? OR abbr = ?", state_name.capitalize, state_name.upcase]).first
-    return unless state
-    self.state = state
+    if state
+      self.state = state
+      self.state_name = nil
+    end
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe Address do
+  # Associations
+  it { should belong_to :country }
+  it { should belong_to :state }
+  it { should have_many :shipments }
+
+  # Validations
+  it { should validate_presence_of :state }
+  it 'should not validate presence of state when a state name is given' do
+    @addr = Address.new @attr.merge(:state_name => 'test', :country => @nostate_country)
+    @addr.should_not validate_presence_of :state
+  end
+  it { should validate_presence_of :state_name }
+  it 'should not validate presence of state name when a state is given' do
+    @addr = Address.new @attr.merge(:state => @state)
+    @addr.should_not validate_presence_of :state_name
+  end
+  it { should validate_presence_of :firstname }
+  it { should validate_presence_of :lastname }
+  it { should validate_presence_of :address1 }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :zipcode }
+  it { should validate_presence_of :country }
+  it { should validate_presence_of :phone }
+
   before :each do
+    @nostate_country = Country.create! :iso_name => 'CA', :iso => 'CA', :name => 'Canada', :iso3 => 'CAN', :numcode => '222'
     @country = Country.create! :iso_name => 'US', :iso => 'US', :name => 'United States', :iso3 => 'USA', :numcode => '111'
     @state = State.create! :name => 'California', :abbr => 'CA', :country_id => @country.id
 
@@ -35,6 +60,21 @@ describe Address do
     it 'should recognize a state abbr ignoring case and convert it to a state object' do
       @addr = Address.create! @attr.merge(:state_name => 'ca')
       @addr.state.should == @state
+    end
+
+    it 'should leave the state alone if a state_id is given' do
+      @addr = Address.create! @attr.merge(:state_id => @state.id)
+      @addr.state.should == @state
+    end
+
+    it 'should not be valid if neither a state or state_name are given' do
+      @addr = Address.new @attr
+      @addr.should_not be_valid
+    end
+
+    it 'should not be valid if the state name isnt mappable to a known state' do
+      @addr = Address.new @attr.merge(:state_name => 'abracadabra')
+      @addr.should_not be_valid
     end
   end
 end
